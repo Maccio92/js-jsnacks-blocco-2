e27f4cbfb34ad16935f92ed1a9572cb9371d88ba
Maccio92 <valerio362@gmail.com>
2021-11-30 11:15:50 +0100

    jsnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,30 +1,24 @@
 /* Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.*/
 
+// Creo 2 array di nomi e o cognomi
 let names = ['aldo', 'giovanni', 'giacomo', 'marina', 'claudia', 'katia']
 console.log(names);
 let surnames = ['baglio', 'storti', 'poretti', 'massironi', 'giangi', 'conti']
 console.log(surnames);
 
-// let randNames = names[Math.floor(Math.random() * names.length)];
-// console.log(randNames);
-
-// let randSurnames = surnames[Math.floor(Math.random() * surnames.length)];
-// console.log(randSurnames);
-
+// Creo l'array vuota per la lista di nomi
 let list = [];
 console.log(list);
 
+// Ciclo per generare la lista random
 for (let index = 0; index < names.length; index++) {
     let randNames = names[Math.floor(Math.random() * names.length)];
 
     let randSurnames = surnames[Math.floor(Math.random() * surnames.length)];
 
-    // console.log(randNames);
-    // console.log(randSurnames);
-    
     let guest = randNames + ` ` + randSurnames;
     console.log(guest);
-
+//  Inserisco gli elementi della lista nell'array
     list.push (guest);
 }
 
